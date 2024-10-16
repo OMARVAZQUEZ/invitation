e95ce5cec108e7e7e611fc25781134643b5f6fdd
OMARVAZQUEZ <omarvazquezgordillo@gmail.com>
2024-10-16 09:05:16 -0600

    landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 // App.js
 import React, { useState } from 'react';
-import Header from './components/components extra/Header.jsx';
-import Countdown from './components/components extra/Countdown.jsx';
-import EventDetails from './components/components extra/EventDetails.jsx';
-import BiblicalQuote from './components/components extra/BiblicalQuote.jsx';
-import ReserveButton from './components/components extra/ReserveButton.jsx';
-import PhotoGallery from './components/components extra/PhotoGallery.jsx';
-import BackgroundMusic from './components/components extra/BackgroundMusic.jsx';
 import WeddingInvitation from './components/WeddingInvitation.tsx';
 import './App.css'; // Importa los estilos
 
