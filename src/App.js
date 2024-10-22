4bb8e4a63e04b9217b96bf3884e1775da16a485d
OMARVAZQUEZ <omarvazquezgordillo@gmail.com>
2024-10-22 16:55:26 -0600

    configure horario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // App.js
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 import WeddingInvitation from './components/WeddingInvitation.tsx';
+
 import './App.css'; // Importa los estilos
 
 const App = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="app">
       <WeddingInvitation />
+    
     </div>
   );
 };
